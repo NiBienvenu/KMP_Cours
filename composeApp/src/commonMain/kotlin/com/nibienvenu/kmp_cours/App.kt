8cbcc0729a1feec9dea6461622707171a51c9e03
NiBienvenu <nicibienvenu@gmail.com>
2025-01-27 12:50:00 +0200

    badges

--- composeApp/src/commonMain/kotlin/com/nibienvenu/kmp_cours/App.kt
+++ composeApp/src/commonMain/kotlin/com/nibienvenu/kmp_cours/App.kt
@@ -1,7 +1,16 @@
 package com.nibienvenu.kmp_cours
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Badge
+import androidx.compose.material.BadgedBox
+import androidx.compose.material.Button
 import androidx.compose.material.ExtendedFloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.SnackbarHost
@@ -9,10 +18,14 @@ import androidx.compose.material.SnackbarHostState
 import androidx.compose.material.SnackbarResult
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.nibienvenu.kmp_cours.Components.BadgeExample
+import com.nibienvenu.kmp_cours.Components.BadgeInteractiveExample
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -68,8 +81,25 @@ fun App() {
                 SnackbarHost(hostState = snackbarHostState)
             },
         ) { innerPadding ->
+
+            Box(
+                modifier = Modifier
+                   .padding(10.dp)
+                   .fillMaxWidth()
+            ){
+
+                Row(
+                    modifier = Modifier.padding(vertical = 16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    BadgeExample()
+                    BadgeInteractiveExample()
+                }
+
+            }
             // Your main content goes here
             // Use innerPadding to apply padding to content
         }
     }
 }
+
--- composeApp/src/commonMain/kotlin/com/nibienvenu/kmp_cours/Components/Badges.kt
+++ composeApp/src/commonMain/kotlin/com/nibienvenu/kmp_cours/Components/Badges.kt
@@ -1,10 +1,63 @@
 package com.nibienvenu.kmp_cours.Components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Badge
+import androidx.compose.material.BadgedBox
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun Badges(){
+fun BadgeExample() {
+    BadgedBox(
+        badge = {
+            Badge()
+        }
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Email,
+            contentDescription = "Email"
+        )
+    }
+}
 
-    // declaration des variables de badges
+@Composable
+fun BadgeInteractiveExample() {
+    var itemCount by remember { mutableStateOf(0) }
 
+    Column (
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        BadgedBox(
+            badge = {
+                if (itemCount > 0) {
+                    Badge(
+                        backgroundColor = Color.Red,
+                        contentColor = Color.White
+                    ) {
+                        Text("$itemCount")
+                    }
+                }
+            }
+        ) {
+            Icon(
+                imageVector = Icons.Filled.ShoppingCart,
+                contentDescription = "Shopping cart",
+            )
+        }
+        Button(onClick = { itemCount++ }) {
+            Text("Add item")
+        }
+    }
 }
